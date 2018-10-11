f95ba7407a229aa04464d35505790b1012dd9925
Michael Acobas <michael.acobas@baschel.io>
2018-10-12 00:12:01 +0300

    write to db

--- guild_drains.py
+++ guild_drains.py
@@ -1,3 +1,9 @@
+'''
+Monthly guild drain script.
+Should be added to crontab to run on the 1st of every month.
+Released under the MIT License by the Open Underlight Project.
+'''
+
 from mysql.connector.cursor import MySQLCursorPrepared
 from collections import defaultdict
 from underlight_api.database import UnderlightDatabase
@@ -11,7 +17,7 @@ def get_guild_drains(db):
     stmt = '''select count(guildplayer.player_id),guild_id,rank from 
         guildplayer,player where 
             guildplayer.player_id = player.player_id and 
-            player.acct_type=80 and 
+            player.acct_type=65 and 
             guildplayer.player_id not in (
                 select distinct g1.player_id from guildplayer g1,guildplayer g2 where 
                     g1.player_id = g2.player_id and 
@@ -53,7 +59,42 @@ def get_prime_info(db):
         })        
     return ret
 
+def write_primes(db,primes,drains):
+    # we're only gonna bother with strength
+    cxn = db.connect(db = 'ul_item')
+    for guild,drain in drains.items():
+        prime_list = primes.get(guild, None)
+        if prime_list is None:
+            print('ERROR: No prime found for guild %d' % guild)
+            continue
+        elif len(prime_list) > 1:
+            print('WARN: More then one prime found for guild %d - draining from all!' % guild)
+        
+        for prime in prime_list:
+            s = prime.get('strength', 0)
+            s -= drain
+            if s < 0:
+                print('WARN: Prime %s (id: %d) for guild %d will underflow: setting to 0!' % (prime['name'], prime['item_id'], guild))
+                s = 0
+            strength_string = "{0:0{1}x}".format(s,8)
+            state_strings = prime['state_strings']
+            s0 = list(state_strings[0])
+            s1 = list(state_strings[1])
+            s1[6:] = strength_string[0:2]
+            s0[0:2] = list(strength_string[2:4])
+            s1[2:6] = list(strength_string[4:])
+            state_strings[0] = ''.join(s0)
+            state_strings[1] = ''.join(s1)
+            print('INFO: Draining %s (old=%d;drain=%d;new=%d)' % (prime['name'], prime['strength'], drain, s))
+            stmt = '''update item set item_state1=0x%s, item_state2=0x%s where item_id=%d''' % (state_strings[0], state_strings[1], prime['item_id'])
+            print('EXECUTING: %s' % stmt)
+            cursor = cxn.cursor(cursor_class=MySQLCursorPrepared)
+            cursor.execute(stmt)
+            cxn.commit()
+            cursor.close()
+
 if __name__ == '__main__':
     db = UnderlightDatabase.get()
     house_primes = get_prime_info(db)
     drains = get_guild_drains(db)
+    write_primes(db,house_primes,drains)
